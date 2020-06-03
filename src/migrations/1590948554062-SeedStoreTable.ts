4aa8d4aa84e8ea715d7c2e697f3b8c5d7db156c1
cdcordobaa <cdcordobaa@unal.edu.co>
2020-06-03 11:21:17 -0500

    migrations issues

--- src/migrations/1590948554062-SeedStoreTable.ts
+++ src/migrations/1590948554062-SeedStoreTable.ts
@@ -10,13 +10,12 @@ import { Store } from "../entities/Store";
 
 export class SeedStoreTable1590948554062 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // const stores = await getRepository("Store").save(StoreSeed);
-    await getConnection()
-      .createQueryBuilder()
-      .insert()
-      .into(Store)
-      .values(StoreSeed)
-      .execute();
+    // await getConnection()
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Store)
+    //   .values(StoreSeed)
+    //   .execute();
   }
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
