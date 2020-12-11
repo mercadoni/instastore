b42d0f4d63dfbfa969d7b1e914f24b18aab68dc2
carlos <Carlos Arias>
2020-12-11 00:55:23 -0500

    Clean and update boilerplate, add controller

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const storesRouter = require('./routes/stores');
 
 const app = express();
 
@@ -12,9 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/stores', storesRouter);
 
 module.exports = app;
---
+++ backend/controllers/storeController.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getClosestStore: function (req, res, next) {
+    res.status(200).json({
+      message: 'Hola Mundo'
+    })
+  }
+}  
