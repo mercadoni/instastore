be464887daa4c26927f756575d002075e2264335
Andres Felipe Monterrosa Alarcon <andresf.monterrosaa@ecci.edu.co>
2021-03-08 23:14:46 -0500

    ADDING DUMMY DATA

---
+++ backend/src/utils/locations.js
@@ -0,0 +1,65 @@
+export const locations = [
+  {
+    storeId: 1,
+    storeName: "Tienda 1",
+    isOpen: null,
+    coordinates: {
+      lat: 4.7446068,
+      lng: -74.1128802,
+    },
+    nextDeliveryTime: null,
+  },
+  {
+    storeId: 2,
+    storeName: "Tienda 2",
+    isOpen: null,
+    coordinates: {
+      lat: 40.7722691,
+      lng: -74.3008176,
+    },
+    nextDeliveryTime: null,
+  },
+  {
+    storeId: 3,
+    storeName: "Tienda 3",
+    isOpen: null,
+    coordinates: {
+      lat: 40.682638,
+      lng: -73.941015,
+    },
+    nextDeliveryTime: null,
+  },
+  {
+    storeId: 4,
+    storeName: "Tienda 4",
+    isOpen: null,
+    coordinates: {
+      lat: 4.7428369,
+      lng: -74.1108802,
+    },
+    nextDeliveryTime: null,
+  },
+  {
+    storeId: 5,
+    storeName: "Tienda 5",
+    isOpen: null,
+    coordinates: {
+      lat: 40.870347,
+      lng: -74.10581,
+    },
+    nextDeliveryTime: null,
+  },
+  {
+    storeId: 6,
+    storeName: "Tienda 6",
+    isOpen: null,
+    coordinates: {
+      lat: 40.7374197,
+      lng: -74.2719785,
+    },
+    nextDeliveryTime: null,
+  },
+];
+
+
+// https://bogota-laburbano.opendatasoft.com/api/records/1.0/search/?dataset=georeferencia-puntual-por-localidad&q=
