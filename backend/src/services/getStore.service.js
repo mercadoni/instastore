d1dc7750211c99f02558ca2960dc2e386bcd28e8
Andres Felipe Monterrosa Alarcon <andresf.monterrosaa@ecci.edu.co>
2021-03-09 04:17:11 -0500

    FINISHING NEXT DELIVERY TIME, ADDING CALCULATION TIME FOR NEXT DAY

--- backend/src/services/getStore.service.js
+++ backend/src/services/getStore.service.js
@@ -20,9 +20,9 @@ export const getStoreProcess = async (request) => {
     );
   });
 
-  store['nextDeliveryTime'] = await getNextDeliveryTime(request,store);
   store['distance'] = nearestLocation.distance;
   store['isOpen'] = checkIsOpen(store);
+  store['nextDeliveryTime'] = await getNextDeliveryTime(request,store);
 
 
   return store;
@@ -39,7 +39,6 @@ let checkIsOpen = (request) => {
 
 let getNextDeliveryTime = async (userCoords, request) =>{
   let timeFormat = 'hh:mm'
-  let actualTime = moment().format(timeFormat)
   let estimatedTime;
   let estimatedTimeFormatted;
 
@@ -48,6 +47,7 @@ let getNextDeliveryTime = async (userCoords, request) =>{
        await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?=&origins=${request.coordinates.lat},${request.coordinates.lng}&destinations=${userCoords.lat}%2C${userCoords.lng}&key=${process.env.GOOGLE_KEY}`)
       .then(res => res.json())
       .then(json => {
+
         let estimatedBikeTime = (json.rows[0].elements[0].duration.value) / 60
         estimatedTime = (request.queuedOrders*20) + estimatedBikeTime
         var mins_num = parseFloat(estimatedTime, 10);
@@ -57,6 +57,14 @@ let getNextDeliveryTime = async (userCoords, request) =>{
         if (hours   < 10) {hours   = "0"+hours;}
         if (minutes < 10) {minutes = "0"+minutes;}
         estimatedTimeFormatted = hours.toString()+':'+minutes.toString()
+
+        if (moment().add("60", 'minutes').format('LTS').isSameOrAfter(moment(request.closingDate, timeFormat))){
+          let tomorrowDate = moment(request.openingDate, "hh:mm:")
+          .add(estimatedTimeFormatted, 'minutes')
+          .format('hh:mm');
+          estimatedTimeFormatted = "The order will be delivered tomorrow in approx. " + tomorrowDate
+        }
+
       });
     } catch (error) {
       console.error(error)
