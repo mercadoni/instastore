b592e33d2ca21505ee36b756c4d284704c35abcc
Andres Felipe Monterrosa Alarcon <andresf.monterrosaa@ecci.edu.co>
2021-03-08 23:14:00 -0500

    ADDING CONFIG FILES AND AUTHOR INFO

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import morgan from 'morgan';
+import pkg from '../package.json';
+import getStore from './routes/getStore.routes';
+
+const app = express();
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.set('pkg', pkg);
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    name: app.get('pkg').name,
+    author: app.get('pkg').author,
+    description: app.get('pkg').description,
+    version: app.get('pkg').version,
+    repository: app.get('pkg').repository
+  });
+});
+
+app.use('/getStore', getStore);
+
+export default app;
