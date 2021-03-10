e453ec290ae2f020a6bd016f18524868919bca0a
jsierrav14 <jsierrav14@gmail.com>
2021-03-10 10:31:21 -0500

    removing node_modules

--- common/build/events/subjects.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum Subjects {
-    TicketCreated = "ticket:created",
-    TicketUpdated = "ticket:updated",
-    OrderCreated = "order:created",
-    OrderCancelled = "order:cancelled",
-    ExpirationComplete = "expiration:Complete",
-    PaymentCreated = "payment:created"
-}
